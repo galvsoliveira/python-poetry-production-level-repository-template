61d703f2ce6b3320c2580513c79bf37baf06b65d
Gustavo Oliveira <g.alvs.oliveira@gmail.com>
2023-09-21 15:12:18 -0300

    feat: add detect-secrets to pre-commit

--- scripts/03_instalar_dependencias_projeto.sh
+++ scripts/03_instalar_dependencias_projeto.sh
@@ -3,7 +3,18 @@
 if [ ! -f pyproject.toml ]; then
     echo 'Creating pyproject.toml ...'
     poetry init
-    poetry add --dev pre-commit
+    poetry add --group dev pre-commit
+    poetry add --group dev detect-secrets
+fi
+
+if [ ! -f .pre-commit-config.yaml ]; then
+    echo 'Creating .pre-commit-config.yaml ...'
+    pre-commit sample-config > .pre-commit-config.yaml
+fi
+
+if [ ! -f .secrets.baseline ]; then
+    echo 'Creating .secrets.baseline ...'
+    detect-secrets scan > .secrets.baseline
 fi
 
 echo 'Installing dependencies and pre-commit ...'
