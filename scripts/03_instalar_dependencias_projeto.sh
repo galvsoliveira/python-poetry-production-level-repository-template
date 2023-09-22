2cc85bf38a01b45c9864b2ae63968a7183035b08
Gustavo Oliveira <g.alvs.oliveira@gmail.com>
2023-09-22 18:16:18 -0300

    fix: add black pylint and fix if condition

--- scripts/03_instalar_dependencias_projeto.sh
+++ scripts/03_instalar_dependencias_projeto.sh
@@ -5,21 +5,24 @@ if [ ! -f pyproject.toml ]; then
     poetry init
     poetry add --group dev pre-commit
     poetry add --group dev detect-secrets
+    poetry add --group dev black
+    poetry add --group dev pylint
     poetry install
-    poetry shell
 fi
 
 if [ ! -f .pre-commit-config.yaml ]; then
     echo 'Creating .pre-commit-config.yaml ...'
-    pre-commit sample-config > .pre-commit-config.yaml
+    poetry run pre-commit sample-config > .pre-commit-config.yaml
 fi
 
 if [ ! -f .secrets.baseline ]; then
     echo 'Creating .secrets.baseline ...'
-    detect-secrets scan > .secrets.baseline
+    poetry run detect-secrets scan > .secrets.baseline
 fi
 
 echo 'Installing dependencies and pre-commit ...'
 poetry install
 poetry run pre-commit install
+
+echo 'Entering the virtual environment...'
 poetry shell
