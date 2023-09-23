b22bc7db5fb2b84eb178313b6bcf6d341d7bcc45
Gustavo Oliveira <g.alvs.oliveira@gmail.com>
2023-09-23 15:49:59 -0300

    feat: add requirements.txt migration script

---
+++ scripts/04_requirements_txt_to_poetry(optional).sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Read the requirements.txt file
+while IFS= read -r requirement
+do
+  # Try to add each requirement to the poetry project
+  if ! poetry add "$requirement"
+  then
+    echo "Failed to add $requirement"
+    exit 1
+  else
+    echo "Successfully added $requirement"
+  fi
+
+done < requirements.txt
+
+# Check if requirements file was read successfully
+if ! IFS= read -r requirement < requirements.txt
+then
+    echo "Failed to read requirements file"
+    exit 1
+fi
+
+echo "All requirements added successfully"
