ecf53fbe7706dc5a633e87b6b6a75b071f2c3b44
Gustavo Alves de Oliveira <95829723+galvsoliveira@users.noreply.github.com>
2023-10-02 23:24:10 -0300

    feat: update scrip 02

--- scripts/02_install_asdf_plugins.sh
+++ scripts/02_install_asdf_plugins.sh
@@ -7,13 +7,20 @@ echo 'Installing Poetry plugin for asdf...'
 asdf plugin-add poetry https://github.com/asdf-community/asdf-poetry.git
 
 # Read the .tool-versions file and install the specified versions
-python_version=$(grep python .tool-versions | awk '{print $2}')
-poetry_version=$(grep poetry .tool-versions | awk '{print $2}')
+if ! python_version=$(grep python .tool-versions | awk '{print $2}'); then
+    echo "Failed to get Python version"
+    exit 1
+fi
 
-echo "Installing Python $python_version..."
-asdf install python "$python_version"
-asdf local python "$python_version"
+if ! poetry_version=$(grep poetry .tool-versions | awk '{print $2}'); then
+    echo "Failed to get Poetry version"
+    exit 1
+fi
 
-echo "Installing Poetry $poetry_version..."
-asdf install poetry "$poetry_version"
-asdf local poetry "$poetry_version"
+echo "Installing Python ${python_version}..."
+asdf install python "${python_version}"
+asdf local python "${python_version}"
+
+echo "Installing Poetry ${poetry_version}..."
+asdf install poetry "${poetry_version}"
+asdf local poetry "${poetry_version}"
